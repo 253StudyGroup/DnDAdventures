b23ff91c6024acf64cdd196b38cfb02145015131
Marslew <35205152+Marslew@users.noreply.github.com>
2020-04-18 16:27:10 -0700

    Update character-creation.py

--- character-creation.py
+++ character-creation.py
@@ -1,23 +1,28 @@
 import pygame
 
-print("Welcome to the character generator!")
-print("What kind of Character would you like to make?")
-print("Please list your preference, with the number attached.")
-print(" 1. Fighter")
-print(" 2. Rogue")
-print(" 1. Wizard")
+print("Welcome to the character generator! There will be many questions ahead.")
+print("What race is your Character?")
 
 print("What Race would you like to be?")
 print("You could be flexible like a human, agile like an elf, or hearty as a dwarf. Pick which you'd prefer.")
+
+print("What class is your Character?")
+print("Please list your preference, with the number attached.")
+print(" 1. Fighter")
+print(" 2. Rogue")
+print(" 3. Wizard")
+
 
 #TODO Take the user choice and code in what would result from this choice. Include racial bonuses, features, and any other
 # impact to their character
 
 print("Excellent. Now we will roll your attribute scores. These will affect how well your character operates in this world.")
 
-#TODO Import the stats generation script. Also, depending on the CLASS chosen, give the user a choice between the "preferred" option
-# or to go their own path. 
+#TODO Import the stats generation script. 
 
+#Pull in dnd_stats.py
+
+#TODO Advanced Feature: Include a "suggested" assignment based on the PHB
 
 #TODO Designate the points from generation into attribute scores along with the racial bonuses. 
 
