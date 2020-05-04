077ccd897e43b1f49f9d697202f8a427f7829d48
Tomawar40 <46391740+Tomawar40@users.noreply.github.com>
2020-05-03 19:51:54 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ import time
 size = 1200, 900 #Sets Window Size
 RED = (255,0,0)
 GRAY = (150,150,150)
+BLACK = (0,0,0)
 
 width, height = size
 
@@ -20,7 +21,12 @@ pygame.init() #Begins the program.
 
 screen = pygame.display.set_mode(size) #Brings up a Display
 #variableName = Rect(X, Y, W, H)
-nameBox = Rect(0,0,300,25)
+nameBox = Rect(1, 1, 300, 15)
+healthBar = Rect(1, 16, 300, 15)
+print(f'x={nameBox.x}, y={nameBox.y}, width={nameBox.w}, height={nameBox.h}')
+print(f'leftBound={nameBox.left}, topBound={nameBox.top}, rightBound={nameBox.right}, bottomBound={nameBox.bottom}')
+print(f'centerLocation={nameBox.center}')
+
 
 
 
@@ -30,9 +36,11 @@ while running: #NECESSARY TO START THE PROGRAM
     for event in pygame.event.get():
         if event.type == QUIT: #LOOP ALLOWS EXIT TO HAPPEN (otherwise you have to force close)
             running = False
-	screen.fill(GRAY)
-	pygame.draw.rect(screen, RED, rect)
 
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, BLACK, nameBox)
+    pygame.draw.rect(screen, RED, healthBar)
+    pygame.display.flip()
 
 
 
